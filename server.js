02b65eb5bca19ee73f020936633a5f2d8759fd41
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-02-14 14:07:59 -0600

    query added

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,22 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'mmcelwee8002@gmail.com',
+        // Your MySQL password
+        password: 'EGRJ!tBH@5$',
+        database: 'election',
+    },
+    console.log('Connected to the election database.')
+);
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
 app.get('/', (req, res) => {
